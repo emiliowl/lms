5c3b579e494283907e6625a3c68312b2ea048abf
Emilio Murta <emilio.murta@msitbrasil.com>
2018-09-09 17:03:46 -0300

    separar models em arquivos diversos

--- main/models/models.py
+++ main/models/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 from datetime import date
+from .pessoa import Pessoa
 
 # Create your models here.
-class Pessoa(models.Model):
-    nome = models.TextField(max_length=255)
-    email = models.TextField(max_length=255)
-    celular = models.TextField(max_length=20)
-    dt_expiracao = models.DateField(default=date(year=1900, month=1, day=1))
-    login = models.TextField(max_length=20, unique=True)
-    senha = models.TextField(max_length=20)
-
 class Coordenador(Pessoa):
     pass
 
